ab895b7b61054ba91564892ba79aff69455ab128
Jonny Dynamo <jonny@teachon.org>
2022-10-26 12:44:28 +0700

    favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e05c2058fe18cfb"; }
+/******/ 		__webpack_require__.h = function() { return "741c330321fb8775"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.cda8a5295e68bf69.hot-update.js
+++ .next/static/webpack/webpack.cda8a5295e68bf69.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b706642d6790f8c1"; }
+/******/ 	__webpack_require__.h = function() { return "741c330321fb8775"; }
 /******/ }();
 /******/ 
 /******/ }
