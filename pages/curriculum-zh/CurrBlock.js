0776a2271b614b568d91e1edd48fc2921813bcb7
Jonny Dynamo <jonny@teachon.org>
2022-10-26 12:24:15 +0700

    fix currblock

--- pages/curriculum-zh/CurrBlock.js
+++ pages/curriculum-zh/CurrBlock.js
@@ -5,12 +5,7 @@ import classes from "./CurrBlock.module.css";
 function CurrBlock(props) {
   return (
     <div className={classes["curr-block"]}>
-      <ExportedImage
-        src={props.image}
-        alt="curriculum images"
-        width="125"
-        height="125"
-      />
+      <img src={props.image} alt="curriculum images" width="125" height="125" />
       <h3>{props.heading}</h3>
       <p>{props.text}</p>
     </div>
